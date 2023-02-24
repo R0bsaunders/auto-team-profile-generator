8f66195ddfd31679ae724c23a4b6f26afec1cac3
Rob S <me@rob-saunders.co.uk>
2023-02-24 19:38:37 +0000

    Engineer and Employee constructors generated and passing all tests. new test written to check constructor works

---
+++ starter/__tests__/Engineer.test.js
@@ -0,0 +1,43 @@
+const Employee = require("../lib/Employee");
+const Engineer = require("../lib/Engineer");
+
+test("Can set GitHUb account via constructor", () => {
+  const testValue = "GitHubUser";
+  const e = new Engineer("Foo", 1, "test@test.com", testValue);
+  expect(e.github).toBe(testValue);
+});
+
+test("getRole() should return \"Engineer\"", () => {
+  const testValue = "Engineer";
+  const e = new Engineer("Foo", 1, "test@test.com", "GitHubUser");
+  expect(e.getRole()).toBe(testValue);
+});
+
+test("Can get GitHub username via getGithub()", () => {
+  const testValue = "GitHubUser";
+  const e = new Engineer("Foo", 1, "test@test.com", testValue);
+  expect(e.getGithub()).toBe(testValue);
+});
+
+test("The engineer should extend the employee class", () =>{
+
+  const engineer = new Engineer();
+  expect(engineer).toBeInstanceOf(Employee);
+});
+
+test("On construction, the base class is intitialised", () => {
+
+  //Arrange
+  const name = "Chris";
+  const id = 200;
+  const email = "email@email.com";
+
+  //Act
+  const engineer = new Engineer(name, id, email);
+
+  //Assert
+  expect(engineer.name).toBe(name);
+  expect(engineer.id).toBe(id);
+  expect(engineer.email).toBe(email);
+
+});
