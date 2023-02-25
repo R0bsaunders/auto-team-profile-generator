9787a98052610dc16879a4c5be1bf6bcfb82302c
Rob S <me@rob-saunders.co.uk>
2023-02-25 20:24:10 +0000

    added the intern choices to inquirer.choices.js

--- starter/lib/inquirer.choices.js
+++ starter/lib/inquirer.choices.js
@@ -1,87 +1,131 @@
 // array of questions for user
-const choices = 
-
-    {
-        manager: [
-            {
-                type: 'input',
-                name: 'name',
-                message: "What is the manager's name?",
-                validate: function(name) {
-        
-                    return validator(name);
-                }
-            },
-        
-            {
-                type: 'input',
-                name: 'id',
-                message: "What is the manager's ID?",
-                validate: function(id) {
-        
-                    return validator(id);
-                }
-            },
-            {
-                type: 'input',
-                name: 'email',
-                message: "What is the manager's email address?",
-                validate: function(email) {
-        
-                    return validator(email);
-                }
-            },
-            {
-                type: 'input',
-                name: 'officeNumber',
-                message: "What is the manager's Office Number?",
-                validate: function(office) {
-        
-                    return validator(office);
-                }
+const choices = {
+    manager: [
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the manager's name?",
+            validate: function(name) {
+    
+                return validator(name);
             }
-        ],
-
-        engineer: [
-            {
-                type: 'input',
-                name: 'name',
-                message: "What is the engineer's name?",
-                validate: function(name) {
-        
-                    return validator(name);
-                }
-            },
-        
-            {
-                type: 'input',
-                name: 'id',
-                message: "What is the engineer's ID?",
-                validate: function(id) {
-        
-                    return validator(id);
-                }
-            },
-            {
-                type: 'input',
-                name: 'email',
-                message: "What is the engineer's email address?",
-                validate: function(email) {
-        
-                    return validator(email);
-                }
-            },
-            {
-                type: 'input',
-                name: 'github',
-                message: "What is the engineer's GitHub username?",
-                validate: function(github) {
-        
-                    return validator(github);
-                }
+        },
+    
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is the manager's ID?",
+            validate: function(id) {
+    
+                return validator(id);
             }
-        ],
-    };
+        },
+
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the manager's email address?",
+            validate: function(email) {
+    
+                return validator(email);
+            }
+        },
+
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: "What is the manager's Office Number?",
+            validate: function(office) {
+    
+                return validator(office);
+            }
+        }
+    ],
+
+    engineer: [
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the engineer's name?",
+            validate: function(name) {
+    
+                return validator(name);
+            }
+        },
+    
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is the engineer's ID?",
+            validate: function(id) {
+    
+                return validator(id);
+            }
+        },
+
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the engineer's email address?",
+            validate: function(email) {
+    
+                return validator(email);
+            }
+        },
+        
+        {
+            type: 'input',
+            name: 'github',
+            message: "What is the engineer's GitHub username?",
+            validate: function(github) {
+    
+                return validator(github);
+            }
+        }
+    ],
+
+    intern: [
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the intern's name?",
+            validate: function(name) {
+    
+                return validator(name);
+            }
+        },
+    
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is the intern's ID?",
+            validate: function(id) {
+    
+                return validator(id);
+            }
+        },
+
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the intern's email address?",
+            validate: function(email) {
+    
+                return validator(email);
+            }
+        },
+        
+        {
+            type: 'input',
+            name: 'school',
+            message: "What is the intern's GitHub username?",
+            validate: function(school) {
+    
+                return validator(school);
+            }
+        }
+    ] 
+};
 
 
 
