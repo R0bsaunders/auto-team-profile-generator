c0ac6db1f7cb12773ba022eb64750e0c855df08b
Rob S <me@rob-saunders.co.uk>
2023-02-25 18:05:12 +0000

    have added all choices for manager and tested int he terminal sucesfully. Next up is trying to store the different types of employee choices in the same const choices

--- starter/lib/inquirer.choices.js
+++ starter/lib/inquirer.choices.js
@@ -2,88 +2,55 @@
 const choices = [
     {
         type: 'input',
-        name: 'manager',
-        message: "Name of employee's manager?",
+        name: 'name',
+        message: "What is the manager's name?",
         validate: function(manager) {
 
             return validator(manager);
         }
     },
+
     {
         type: 'input',
-        name: 'description',
-        message: 'What is the description of your project?',
-        validate: function(desc) {
+        name: 'id',
+        message: "What is the manager's ID?",
+        validate: function(id) {
 
-            return validator(desc);
+            return validator(id);
         }
     },
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'What are the installation instructions?',
-        validate: function(inst) {
-
-            return validator(inst);
-        }
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'What are the usage instructions?',
-        validate: function(use) {
-
-            return validator(use);
-        }
-    },
-    {
-        type: 'list',
-        name: 'license',
-        message: 'Select a license Type',
-        choices:
-        [
-            'Apache',
-            'Boost',
-            'MIT'
-        ]
-    },
-    {
-        type: 'input',
-        name: 'contributing',
-        message: 'What are the contributing instructions?',
-        validate: function(contr) {
-
-            return validator(contr);
-        }
-    },
-    {
-        type: 'input',
-        name: 'tests',
-        message: 'What tests have been run?',
-        validate: function(test) {
-
-            return validator(test);
-        }
-      },
-      {
-        type: 'input',
-        name: 'github',
-        message: 'What is your GitHub username?',
-        validate: function(git) {
-
-            return validator(git);
-        }
-        },
     {
         type: 'input',
         name: 'email',
-        message: 'What is your email address?',
+        message: "What is the manager's email address?",
         validate: function(email) {
 
             return validator(email);
         }
+    },
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: "What is the manager's Office Number?",
+        validate: function(office) {
+
+            return validator(office);
+        }
     }
-  ];
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Function to check for empty answers and return message prompting user to type something
 function validator(input) {
